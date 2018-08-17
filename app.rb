fac31d9781a905de0db20be2d51b425968d1af7e
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-17 15:58:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   end
 
   post '/new' do
-    @puppy = Puppy.new
+    @puppy = Puppy.new(:name, :breed, :age)
     @puppy.name = params[:name]
     @puppy.breed = params[:breed]
     @puppy.age = params[:age]
